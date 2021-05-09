ed12f97cba101c6693cb7e3a28de5ca95fde26d2
Mohsen Esmailpour <mo.esmp@gmail.com>
2021-05-10 02:27:47 +0430

    Move shared classes to shared project.

---
+++ JobQueue.Shared/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JobQueue.Shared
+{
+    public class MessageModel
+    {
+        public DateTime CreateDate { get; set; }
+
+        public Guid MessageId { get; set; }
+
+        public string Entity { get; set; }
+
+        public string Category { get; set; }
+
+        public object Payload { get; set; }
+    }
+}
---
+++ JobQueue.Shared/MessageProcessor.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace JobQueue.Shared
+{
+    public class MessageProcessor
+    {
+        [Queue("express")]
+        [DisplayName("JobId: {1}")]
+        [AutomaticRetry(Attempts = 3)]
+        public static async Task ProcessExpressMessageAsync(MessageModel message, Guid messageId)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(new Random().Next(1, 4)));
+        }
+
+        [Queue("normal")]
+        [DisplayName("JobId: {1}")]
+        [AutomaticRetry(Attempts = 3)]
+        public static async Task ProcessNormalMessageAsync(MessageModel message, Guid messageId)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(new Random().Next(1, 3)));
+        }
+    }
+}
